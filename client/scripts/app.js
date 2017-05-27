af387aa4dc63d003054b53ef03b7006a4a712b77
Alex Jungroth <aajungroth@ucdavis.edu>
2017-05-27 17:55:13 +0000

    Wrote send and fetch methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,24 +1,40 @@
-var app = $(document).ready(function(){
+var app = $(document).ready(function() {
+  //var $text = $('.text').find(input).val();
+  //var $text = 'test message'
+  var username = window.location.search;
   var $message = {
-    username: '',
-    text: '',
-    roomname: ''
+    username: 'testname',
+    text: $text,
+    roomname: 'test'
   };
-  var $username;
-  var $text;
-  var $roomname;
+  var $username, $text, $roomname;
 
 });
 
-app.stringifyMessage = function(message) {
-  return JSON.stringify(message);
+app.server='http://parse.hrr.hackreactor.com/chatterbox/classes/messages';
+
+//username = href
+//text -- input to string?
+//roomname.append to message
+$('.send').on('click', function(){
+
+});
+var testMessage = {
+  username: 'test',
+  text: 'ipsum lorem',
+  roomname: 'test'
 };
 
 app.send = function(message){
+  /*
+  $.post("http://parse.hrr.hackreactor.com/chatterbox.classes/messages",
+    message
+  );
+  */
   $.ajax({
-    url: 'http://parse.HRR.hackreactor.com/chatterbox.classes/messages',
+    url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
     type: 'POST',
-    DATA: JSON.stringify(message),
+    data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
@@ -29,4 +45,22 @@ app.send = function(message){
     }
   });
 };
-app.stringifyMessage($message);
+app.send(testMessage);
+
+
+app.fetch = function(message) {
+  $.ajax({
+    url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message received');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to receive message', data);
+    }
+  });
+};
+app.fetch();

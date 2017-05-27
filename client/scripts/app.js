6d5a535a3b130680b2d66af2aecfc4077df0382c
Alex Jungroth <aajungroth@ucdavis.edu>
2017-05-27 02:45:20 +0000

    Added boostrap, made basic html structure, app.js can send messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,32 @@
-// YOUR CODE HERE:
+var app = $(document).ready(function(){
+  var $message = {
+    username: '',
+    text: '',
+    roomname: ''
+  };
+  var $username;
+  var $text;
+  var $roomname;
+
+});
+
+app.stringifyMessage = function(message) {
+  return JSON.stringify(message);
+};
+
+app.send = function(message){
+  $.ajax({
+    url: 'http://parse.HRR.hackreactor.com/chatterbox.classes/messages',
+    type: 'POST',
+    DATA: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+app.stringifyMessage($message);
